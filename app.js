b930194dcb13b344d222d59b9493b4ed524b2775
wei0328 <weichchang0328@gmail.com>
2023-10-26 03:56:41 -0400

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express=require('express')
+const mongoose=require('mongoose')
 const app=express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo-list')
+
+const db=mongoose.connection
+
+db.on('error',()=>{
+    console.log('mongodb error!')
+})
+
+db.once('open',()=>{
+    console.log('mongodb connected!')
+})
 
 app.get('/',(req,res)=>{
     res.send('hello!!')
