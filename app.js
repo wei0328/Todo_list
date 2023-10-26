b47bcd4298b7a73f1d7c8981426f066a5e988efd
wei0328 <weichchang0328@gmail.com>
2023-10-26 02:33:24 -0400

    feat:project init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send('hello!!')
+})
+
+app.listen(3000,()=>{
+    console.log('app is running on http://localhost:3000')
+})
