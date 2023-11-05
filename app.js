d0b371a7015e2df046f1762acc6c99811f3a1f81
wei0328 <weichchang0328@gmail.com>
2023-11-05 18:26:49 -0500

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const exphbs  = require('express-handlebars');
+const Todo=require('./models/todo')// 載入 Todo model
 const port=3000;
 
 
@@ -23,7 +24,10 @@ db.once('open',()=>{
 })
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    Todo.find()// 取出 Todo model 裡的所有資料
+        .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(todos=>res.render('index',{todos:todos}))// 將資料傳給 index 樣板
+        .catch(error=>console.error(error))// 錯誤處理
 })
 
 app.listen(3000,()=>{
