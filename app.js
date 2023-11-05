74d98f5de0b80f86754d548bcaa0f4313b0f75fa
wei0328 <weichchang0328@gmail.com>
2023-11-05 17:48:58 -0500

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express=require('express')
-const mongoose=require('mongoose')
+const express=require('express');
+const mongoose=require('mongoose');
+const exphbs  = require('express-handlebars');
+const port=3000;
+
+
 const app=express()
+
+// 设置模板引擎
+app.engine('.hbs', exphbs({defaultLayout: 'main',extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 mongoose.connect('mongodb://127.0.0.1:27017/todo-list')
 
@@ -15,7 +23,7 @@ db.once('open',()=>{
 })
 
 app.get('/',(req,res)=>{
-    res.send('hello!!')
+    res.render('index')
 })
 
 app.listen(3000,()=>{
