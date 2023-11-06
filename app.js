50d2c5b79049fbbe3c416afb9b92588b359c6dd4
wei0328 <weichchang0328@gmail.com>
2023-11-05 20:51:33 -0500

    feat: add detail page

--- app.js
+++ app.js
@@ -31,12 +31,20 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
     Todo.find()// 取出 Todo model 裡的所有資料
         .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-        .then(todos=>res.render('index',{todos:todos}))// 將資料傳給 index 樣板
+        .then(todos=>res.render('index',{todos}))// 將資料傳給 index 樣板
         .catch(error=>console.error(error))// 錯誤處理
 })
 
 app.get('/todos/new',(req,res)=>{
     return res.render('new')
+})
+
+app.get('/todos/:id',(req,res)=>{
+    const id=req.params.id
+    return Todo.findById(id)
+        .lean()
+        .then((todo)=>res.render('detail',{todo}))
+        .catch(error=>console.error(error))
 })
 
 app.post('/todos',(req,res)=>{
